86712d75deec9093812ec3187d930a4ffe92d328
nobleed <111716422+nobleed@users.noreply.github.com>
2024-04-05 13:07:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oGDHb6P65Q65J0NqzY2MqQpbuZVQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	cn0tfGybQjoTTZAW6qZ4x4Sfewo6F_y9LDATDGUppoo',
+      useTemplateId: 'cn0tfGybQjoTTZAW6qZ4x4Sfewo6F_y9LDATDGUppoo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-16',
       festivals: [
